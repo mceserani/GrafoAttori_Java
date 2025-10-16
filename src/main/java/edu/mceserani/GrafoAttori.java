4feb48203196e6e359232bd436d117eb03544a40
Matteo Ceserani <ceserani@gmail.com>
2025-10-16 19:17:02 +0200

    First commit

---
+++ src/main/java/edu/mceserani/GrafoAttori.java
@@ -0,0 +1,18 @@
+package edu.mceserani;
+
+public class GrafoAttori {
+	
+	private GrafoAttori(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author 
+	 * @version 
+	 */
+    public static void main(String[] args){
+       
+    }
+
+}
---
+++ src/test/java/edu/mceserani/GrafoAttoriTest.java
@@ -0,0 +1,20 @@
+package edu.mceserani;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for GrafoAttori.
+ */
+class GrafoAttoriTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testGrafoAttori() {
+        assertEquals(1, 1);
+    }
+
+}
